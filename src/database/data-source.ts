747f575f53732b3f656ffa4c90ec0320301c9d5d
Mateus Vinicius da Silva <mateusviniciusdasilva@outlook.com>
2022-06-19 10:32:39 -0300

    database entities creation

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,12 +1,15 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 
+import { SignUp } from "./entity/SignUp";
+import { Tweets } from "./entity/Tweets";
+
 export const AppDataSource = new DataSource({
   type: "sqlite",
   database: "projeto_11_tweteroo",
   synchronize: true,
   logging: false,
-  entities: [],
+  entities: [SignUp, Tweets],
   migrations: ["./src/database/migration/*.ts"],
   subscribers: [],
 });
